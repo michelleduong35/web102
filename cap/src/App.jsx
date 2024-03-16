f9ad61b7df0bac77eb331ad61467acda56576d41
michelleduong35 <michelleduong35@gmail.com>
2024-03-16 11:58:17 -0700

    Lab 4 Completed
    Cap! Screenshoting Site

---
+++ cap/src/App.jsx
@@ -0,0 +1,109 @@
+import { useState } from 'react'
+import './App.css'
+import APIForm from '/components/APIForm';
+import Gallery from '/components/Gallery';
+
+function App() {
+  const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+  const [currentImage, setCurrentImage] = useState(null);
+  const [prevImages, setPrevImages] = useState([]);
+
+
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const reset = () => {
+    setInputs({
+      url: "",
+      format: "",
+      no_ads: "",
+      no_cookie_banners: "",
+      width: "",
+      height: "",
+    });
+  }
+
+  const callAPI = async (query) => {
+    const response = await fetch(query);
+    const json = await response.json();
+    console.log(json);
+    if (json.url == null) {
+      alert("Oops! Something went wrong with that query, let's try again!")
+        }
+    else {
+      setCurrentImage(json.url);
+      setPrevImages((images) => [...images, json.url]);
+      reset();
+    }
+  }
+
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+    callAPI(query).catch(console.error);
+  }
+
+  const submitForm = () => {
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit an url!");
+    } else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value == "") {
+          inputs[key] = defaultValues[key]
+        }
+      }
+      makeQuery();
+    }
+
+  }
+  return (
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+      
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        onSubmit={submitForm}
+      />
+      <div className="container">
+  <Gallery images={prevImages} />
+</div>
+      {currentImage ? (
+  <img
+    className="screenshot"
+    src={currentImage}
+    alt="Screenshot returned"
+  />
+) : (
+  <div> </div>
+)}
+      <br></br>
+    
+    </div>
+    
+  );
+}
+
+export default App
